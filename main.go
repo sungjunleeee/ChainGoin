326305e7fe29b6fd7bbd731b2fbcbee78317f6ea
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-25 19:52:33 -0500

    Handling POST request
    for adding new blocks

--- main.go
+++ main.go
@@ -26,11 +26,25 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "home", data)
 }
 
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(w, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+	}
+
+}
+
 func main() {
 	templates = template.Must(template.ParseGlob(templatePath + "pages/*.gohtml"))
 	// updating the existing templates variable to include the partials
 	templates = template.Must(templates.ParseGlob(templatePath + "partials/*.gohtml"))
 	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/add", handleAdd)
 	fmt.Println("Server is running on http://localhost" + port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
