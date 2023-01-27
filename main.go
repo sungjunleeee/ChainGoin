128266bb688fa7814c20b19b8c774d9baea139da
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-26 19:30:20 -0500

    Introducing CLI

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"github.com/sungjunleeee/juncoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to Juncoin CLI\n")
+	fmt.Printf("Instructions:\n")
+	fmt.Printf("html: 	Start HTML explorer\n")
+	fmt.Printf("rest:	Start REST APIs (recommended)\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+	switch os.Args[1] {
+	case "html":
+		fmt.Println("Starting HTML explorer")
+	case "rest":
+		fmt.Println("Starting REST APIs")
+	default:
+		usage()
+	}
 }
