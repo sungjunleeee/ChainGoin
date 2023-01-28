4f644fa5709228fea24d7736f49c6fcff18f84b5
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-28 18:48:48 -0500

    Implemented complete CLI

--- main.go
+++ main.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+	"github.com/sungjunleeee/juncoin/cli"
 )
 
-func usage() {
-	fmt.Printf("Welcome to Juncoin CLI\n")
-	fmt.Printf("Instructions:\n")
-	fmt.Printf("html: 	Start HTML explorer\n")
-	fmt.Printf("rest:	Start REST APIs (recommended)\n")
-	os.Exit(0)
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	switch os.Args[1] {
-	case "html":
-		fmt.Println("Starting HTML explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
+	cli.Start()
 }
